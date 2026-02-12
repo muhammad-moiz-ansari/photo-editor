4a949abddabd5dd7e51be2bb522286ab6532e7f9
Moiz Ansari <moizansari2005@gmail.com>
2026-02-12 11:45:03 +0500

    Added Load Image

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -1,17 +1,57 @@
+chooseBtn = document.querySelector(".choose-btn");
+resetBtn = document.querySelector(".reset-btn");
+saveBtn = document.querySelector(".save-btn");
 
+const canvas = document.getElementById("canvas-image");
+const context = canvas.getContext("2d");
+const fileInput = document.getElementById("file-input")
+
+let originalImage = new Image();
+
+// Load default image
 window.onload = function() {
-    const canvas = document.getElementById("canvas-image");
-    const context = canvas.getContext("2d");
-    const img = new Image();
-
-    img.src="default.png";
-    img.cover = true;
-    img.width = canvas.clientWidth;
-    img.height = canvas.clientHeight;
-    img.onload = function() {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        context.drawImage(img, 0, 0, canvas.clientWidth, canvas.clientHeight);
+    originalImage.src="default.png";
+    originalImage.cover = true;
+    originalImage.width = canvas.clientWidth;
+    originalImage.height = canvas.clientHeight;
+    originalImage.onload = function() {
+        canvas.width = originalImage.width;
+        canvas.height = originalImage.height;
+        context.drawImage(originalImage, 0, 0, canvas.clientWidth, canvas.clientHeight);
     }
+
+    chooseBtn.addEventListener("click", () => {
+        fileInput.click();
+    });
+
+    fileInput.addEventListener("change", function() {
+        const file = this.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+                originalImage.src = event.target.result;
+                
+            }
+            reader.readAsDataURL(file);
+        }
+    });
 }
 
+// Select Image
+function selectImage() {
+    fileInput.addEventListener("change", function() {
+        const file = this.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function() {
+                originalImage.src = reader.result;
+                originalImage.onload = function() {
+                    canvas.width = originalImage.width;
+                    canvas.height = originalImage.height;
+                    context.drawImage(originalImage, 0, 0, canvas.clientWidth, canvas.clientHeight);
+                }
+            }
+            reader.readAsDataURL(file);
+        }
+    });
+}
